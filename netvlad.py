cc54b08d875a16a7fbe13ab93d1993f0a4dc52fe
Zhen Lee <zhenlee105@gmail.com>
2017-08-21 21:11:35 +0800

    trainable

--- netvlad.py
+++ netvlad.py
@@ -21,18 +21,21 @@ class Netvlad:
         """
 
         self.conv1 = self.conv_layer(rgb, 3, 64, "conv1")
-        self.pool1 = self.max_pool(self.conv1, 'pool1')
+        self.norm1 = tf.nn.lrn(self.conv1, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75)
+        self.pool1 = self.max_pool(self.norm1, 'pool1')
 
         self.conv2_1 = self.conv_layer(self.pool1, 64, 64, "conv2_1")
         self.conv2_2 = self.conv_layer(self.conv2_1, 64, 64, "conv2_2")
         self.conv2_3 = self.conv_layer(self.conv2_2, 64, 64, "conv2_3")
         self.conv2_4 = self.conv_layer(self.conv2_3, 64, 64, "conv2_4")
-        self.pool2 = self.max_pool(self.conv2_4, 'pool2')
+        self.norm2 = tf.nn.lrn(self.conv2_4, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75)
+        self.pool2 = self.max_pool(self.norm2, 'pool2')
 
         self.conv3_1 = self.conv_layer(self.pool2, 64, 128, "conv3_1")
         self.conv3_2 = self.conv_layer(self.conv3_1, 128, 128, "conv3_2")
         self.conv3_3 = self.conv_layer(self.conv3_2, 128, 128, "conv3_3")
         self.conv3_4 = self.conv_layer(self.conv3_3, 128, 128, "conv3_4")
+        self.norm3 = tf.nn.lrn(self.conv3_4, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75)
         self.pool3 = self.max_pool(self.conv3_4, 'pool3')
 
         self.conv4_1 = self.conv_layer(self.pool3, 128, 256, "conv4_1")
@@ -62,8 +65,7 @@ class Netvlad:
 
             conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding = 'SAME')
             bias = tf.nn.bias_add(conv, conv_biases)
-            norm = tf.nn.lrn(bias, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75)
-            relu = tf.nn.relu(norm)
+            relu = tf.nn.relu(bias)
 
             return relu
 
