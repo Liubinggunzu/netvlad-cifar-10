1afdf3e3956734c9a05423c2b609707db3c3970b
Zhen Lee <zhenlee105@gmail.com>
2017-08-25 17:44:31 +0800

    a-softmax

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def main(_):
             loss, output1, output2 = A_softmax.A_softmax(model.fc2, tf.one_hot(Y, depth = 10), model.fc3, 4)
             train = tf.train.GradientDescentOptimizer(FLAGS.lr).minimize(loss)
 
-            # output = tf.nn.softmax(model.fc3)[0, :]
+            output3 = model.fc2
 
             correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(model.fc3), axis = 1), Y)
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
@@ -56,11 +56,12 @@ def main(_):
                 count = 0.0
                 for x, y in train_utils.next_batch(FLAGS.batch_size, 'cifar-10-batches-py'):
                     count += 1
-                    _, train_loss, acc, out1, out2 = sess.run([train, loss, accuracy, output1, output2], feed_dict = {X: x, Y: y})
+                    _, train_loss, acc, out1, out2, out3 = sess.run([train, loss, accuracy, output1, output2, output3], feed_dict = {X: x, Y: y})
                     if count % FLAGS.print_every == 0:
                         print("Epoch: %s    progress: %.4f  accuracy = %.4f      training_loss = %.6f\n" % (i, count / numBatch, acc, train_loss))
                         print(out1[:10])
                         print(out2[:10])
+                        print(out3[:10, 0])
                 if (i + 1) % FLAGS.save_every == 0:
                     model.save_npy(sess, "%s/epoch_%d_loss_%.6f" % (FLAGS.checkpoint_dir, i, train_loss))
                 
