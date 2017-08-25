3eef258d02f631f52727907664a83839357712a7
Zhen Lee <zhenlee105@gmail.com>
2017-08-25 17:52:09 +0800

    a-softmax

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def main(_):
                         print("Epoch: %s    progress: %.4f  accuracy = %.4f      training_loss = %.6f\n" % (i, count / numBatch, acc, train_loss))
                         print(out1[:10])
                         print(out2[:10])
-                        print(out3[:10, 0])
+                        print(out3[:10, 0, 0, 0])
                 if (i + 1) % FLAGS.save_every == 0:
                     model.save_npy(sess, "%s/epoch_%d_loss_%.6f" % (FLAGS.checkpoint_dir, i, train_loss))
                 
