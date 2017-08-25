a0d68f14db3abfdc8f71f33907d7bda6818d92a6
Zhen Lee <zhenlee105@gmail.com>
2017-08-25 11:18:15 +0800

    a-softmax

--- A_softmax.py
+++ A_softmax.py
@@ -30,7 +30,7 @@ def A_softmax(x, y, W_norm, fc, m, batch_size, numClass):
     # print(D.get_shape())
     fc_softmax = tf.nn.softmax(F)
     loss = tf.reduce_sum(-tf.log(fc_softmax) * y)
-    return loss, K
+    return loss, cos_thelta
 
 def func_thelta(cos_thelta, m, batch_size):
     if m == 2:
