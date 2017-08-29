eb9e20a56829f1eca717dd0955b2ccdac6302719
Zhen Lee <zhenlee105@gmail.com>
2017-08-29 16:13:52 +0800

    a-softmax

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def main(_):
                         # print(out1[:10])
                         # print(out2[:10])
                         # print(out3[:10, 0])
-                        print [list(output).count(j) fot j in range(10)]
+                        print [list(output).count(j) for j in range(10)]
                             
                 if (i + 1) % FLAGS.save_every == 0:
                     model.save_npy(sess, "%s/epoch_%d_loss_%.6f" % (FLAGS.checkpoint_dir, i, train_loss))
