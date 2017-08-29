f5713c24d6d79558c582eaf1298b0f05582b3441
Zhen Lee <zhenlee105@gmail.com>
2017-08-29 10:33:34 +0800

    a-softmax

--- train.py
+++ train.py
@@ -19,6 +19,7 @@ tf.app.flags.DEFINE_float('lr', 0.001, 'initial learning rate')
 tf.app.flags.DEFINE_integer('print_every', 5, 'print every ... batch')
 tf.app.flags.DEFINE_integer('save_every', 5, 'save model every ... epochs')
 tf.app.flags.DEFINE_integer('m', 1, 'margin of angular softmax')
+tf.app.flags.DEFINE_boolean('use_a_softmax', True, 'use angular softmax or not')
 
 tf.app.flags.DEFINE_boolean('use_vlad', True, 'use vlad pooling or not')
 FLAGS = tf.app.flags.FLAGS
@@ -38,12 +39,11 @@ def main(_):
             model.build(X)
             print("number of total parameters in the model is %d\n" % model.get_var_count())
 
-
-            loss, output1, output2 = A_softmax.A_softmax(model.fc2, tf.one_hot(Y, depth = 10), model.fc3, FLAGS.m)
-            # loss = tf.reduce_mean(-tf.log(tf.nn.softmax(model.fc3)) * tf.one_hot(Y, depth = 10))
+            if FLAGS.use_a_softmax:
+                loss = A_softmax.A_softmax(model.fc2, tf.one_hot(Y, depth = 10), model.fc3, FLAGS.m)
+            else:
+                loss = tf.reduce_mean(-tf.log(tf.nn.softmax(model.fc3)) * tf.one_hot(Y, depth = 10))
             train = tf.train.RMSPropOptimizer(FLAGS.lr).minimize(loss)
-
-            output3 = model.fc3
 
             correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(model.fc3), axis = 1), Y)
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
@@ -58,13 +58,13 @@ def main(_):
                 count = 0.0
                 for x, y in train_utils.next_batch(FLAGS.batch_size, 'cifar-10-batches-py'):
                     count += 1
-                    _, train_loss, acc, out1, out2, out3 = sess.run([train, loss, accuracy, output1, output2, output3], feed_dict = {X: x, Y: y})
+                    _, train_loss, acc = sess.run([train, loss, accuracy], feed_dict = {X: x, Y: y})
                     # _, train_loss, acc, out3 = sess.run([train, loss, accuracy, output3], feed_dict = {X: x, Y: y})
                     if count % FLAGS.print_every == 0:
                         print("Epoch: %s    progress: %.4f  accuracy = %.4f      training_loss = %.6f\n" % (i, count / numBatch, acc, train_loss))
-                        print(out1[:10])
-                        print(out2[:10])
-                        print(out3[:10, 0])
+                        # print(out1[:10])
+                        # print(out2[:10])
+                        # print(out3[:10, 0])
                 if (i + 1) % FLAGS.save_every == 0:
                     model.save_npy(sess, "%s/epoch_%d_loss_%.6f" % (FLAGS.checkpoint_dir, i, train_loss))
                 
